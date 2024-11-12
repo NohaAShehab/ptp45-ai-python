db2bd2c5a8cc40d8e4964126abf2081654764efa
Noha Shehab <nohashehab.iti@gmail.com>
2024-11-12 12:30:23 +0200

    introduction to python

---
+++ intro.py
@@ -0,0 +1,90 @@
+
+# this is a comment
+# 1- print hello world
+print("Hello world ")
+
+# define a variable
+name= 'Ali Ayman'
+grade = 100
+
+# interpreter --> detect datatype in the run time
+
+# if=  "noha" # SyntaxError: invalid syntax
+########################
+# name = 'noha'
+# print(name)
+# print="Abbass"
+
+#######
+name=  'noha'
+track = "python"
+# bio = ("My name is Noha"
+#        "I works at ITI "
+#        "I lives in Mansoura ")
+#
+# print(bio)
+
+# bio = ("My name is Noha\n"
+#        "I works at ITI\n"
+#        "I lives in Mansoura ")
+#
+# print(bio)
+
+
+####
+
+
+#
+# bio= '''My name is Noha
+# I works at ITI
+# I lives in Mansoura.'''
+#
+# print(bio)
+
+###3 developers use string without reference as a comment
+
+# '''
+#     this my comment
+#     these lines will be executed
+# '''
+# print("0----")
+
+
+
+
+# message = "we love python"  # o --> @
+# newmsg = ''
+# for char in message:
+#     if char=='o':
+#         newmsg += '@' # reassign  # newmsg= newmsg + '@'
+#     else:
+#         newmsg += char
+# print(newmsg)
+# print("-------------------")
+
+
+
+
+#### input function
+salary = input("please enter your salary: ")
+print(salary, type(salary))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
