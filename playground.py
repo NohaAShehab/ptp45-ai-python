4c097d678ffe85e16f221dd17a0c2ccddcee73d5
Noha Shehab <nohashehab.iti@gmail.com>
2024-11-14 12:56:37 +0200

    list, tuple, ranges

---
+++ playground.py
@@ -0,0 +1,29 @@
+
+num = 32
+
+num3 = 43234.234
+
+print(type(num3))
+
+### numbers , get min , max
+
+a,b,c,d, e, z = 10,24,424,-223, "anystring", [4,5653,234]
+
+
+print(min(a,b,c,d))
+
+# round
+# print(round(num3))
+
+# numm = 324234.876746
+# print(numm, round(numm))
+
+numm = 324234.876746
+print(numm, round(numm, 2))
+
+### complex number
+c = 4+5j
+print(c)
+
+
+indexed_list = enumerate(myl)
