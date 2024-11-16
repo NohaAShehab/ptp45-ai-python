e25fdaaf28aa37f52da8df0af64ad90a4f4203c1
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-11-16 13:08:39 +0200

    dict, sets, functions, scopes

---
+++ playground.py
@@ -0,0 +1,21 @@
+
+
+# sets
+
+# 1- define a set
+mys = set()
+
+
+# 2- set can hold different data from different data types
+mys = {"iti", "Ahmed", "Menna", 'ali', "iti", 'iti', 323,2432.333, True , None}
+print(mys)
+# items not repeated
+# item not ordered
+# no index
+
+l = [34,2233,233]
+
+print(3,5,3)
+
+mmm= "{0}"
+print(mmm.format("jkhjk"))
